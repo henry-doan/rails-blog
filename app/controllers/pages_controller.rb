2ef8f41d130bfe931c6f05e749bb3ceb367865d7
Henry Doan <henrydoan96@gmail.com>
2018-11-19 21:02:26 -0700

    finished and got really confused

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,21 +1,27 @@
 class PagesController < ApplicationController
+  # CRUD
+
+  # Read, views all the pages
   def index
     # SQL SELECT * FROM PAGES
     @pages = Page.all
     # render views
   end
 
+  # Read, views a single page
   def show
     @page = Page.find(params[:id])
     # render 
   end
 
+  # Read, renders the form for a new page
   def new
     # creates a new page only in memory
     @page = Page.new
     # renders the form
   end
 
+  # Create , create a new page
   def create
     # Page.create(Title: 'dfasd', body: 'afasdfasdf')
     @page = Page.new(pages_params)
@@ -27,6 +33,34 @@ class PagesController < ApplicationController
     end
   end
 
+  # Read, renders the edit form
+  def edit
+    @page = Page.find(params[:id])
+    # render the edit form
+  end
+
+  # Update, updates a page
+  def update
+    # Looking for a page a specific id
+    @page = Page.find(params[:id])
+    
+    # updates with new info
+    if @page.update(pages_params)
+      redirect_to pages_path
+    else
+      render :edit
+    end
+  end
+
+  # Delete , delete a page
+  def destroy
+    # find and delete a page
+    Page.find(params[:id]).destroy
+
+    # take us back to all pages
+    redirect_to pages_path
+  end
+  
   private
 
     def pages_params
---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,9 @@
+class StaticPagesController < ApplicationController
+  def home
+    # Renders Home views
+  end
+
+  def about
+    # Renders About views
+  end
+end
